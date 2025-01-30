3a54c25347ce2194238af8cad66b78f1aa22e26a
Letruke-007 <letruke@mail.ru>
2025-01-30 16:38:37 +0000

    fixed errors

--- src/classes/Character.js
+++ src/classes/Character.js
@@ -3,7 +3,7 @@ import CharacterTypes from '../data/CharacterTypes';
 const characterTypes = Object.values(CharacterTypes);
 
 export default class Character {
-  constructor(name, type) {
+  constructor(name, type, health = 100, level = 1, attack = 0, defence = 0) {
     if (typeof name === 'string' && name.length >= 2 && name.length <= 10) {
       this.name = name; // имя
     } else {
@@ -16,9 +16,27 @@ export default class Character {
       throw new Error(`Персонаж должен быть одного из типов: ${characterTypes.join(', ')}`);
     }
 
-    this.health = 100; // уровень жизни
-    this.level = 1; // уровень персонажа
-    this.attack = undefined; // атака
-    this.defence = undefined; // защита
+    this.health = health; // уровень жизни
+    this.level = level; // уровень персонажа
+    this.attack = attack; // атака
+    this.defence = defence; // защита
+  }
+
+  levelUp() {
+    if (this.health > 0) {
+      this.health = 100;
+      this.level += 1;
+      this.attack *= 1.2;
+      this.defence *= 1.2;
+    } else {
+      throw new Error('Нельзя повысить уровень умершего');
+    }
+  }
+
+  // points - это урон, наносимый персонажу
+  damage(points) {
+    if (this.health >= 0) {
+      this.health -= points * (1 - this.defence / 100);
+    }
   }
 }
--- src/classes/__tests__/Character.test.js
+++ src/classes/__tests__/Character.test.js
@@ -21,7 +21,32 @@ test('Testing character creation', () => {
     type: 'Bowman',
     health: 100,
     level: 1,
-    attack: undefined,
-    defence: undefined,
+    attack: 0,
+    defence: 0,
   });
+});
+
+test('Testing the character\'s levelUp function, where we managed to increase the character level', () => {
+  const character = new Character('character', CharacterTypes.bowman, 50, 50, 50, 50);
+  character.levelUp();
+
+  expect(character).toEqual({
+    name: 'character', type: CharacterTypes.bowman, health: 100, level: 51, attack: 60, defence: 60,
+  });
+});
+
+test('Testing the character\'s levelUp function, where it was not possible to increase the character level', () => {
+  const character = new Character('character', CharacterTypes.bowman, 0, 50, 50, 50);
+
+  expect(() => character.levelUp()).toThrow(Error);
+});
+
+test.each([
+  ['changes', 50, 45],
+  ['does not change', -1, -1],
+])('Testing the character\'s damage function, where the character\'s life %s', (_, health, expected) => {
+  const character = new Character('character', CharacterTypes.bowman, health, 50, 50, 50);
+  character.damage(10);
+
+  expect(character.health).toBe(expected);
 });
